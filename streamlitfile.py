3d7c6184e7d5037e3cf93d8c9f136c537487de48
sriram <sriram2703007@gmail.com>
2026-03-24 20:21:35 +0530

    Enhance streamlitfile.py with comments for clarity
    Added comments to clarify data loading, caching, and UI functionality.

--- streamlitfile.py
+++ streamlitfile.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import txtai
-
+#loading and saving of data
 def text_search():
     np.random.seed(1)
     df=pd.read_csv("train.csv")
@@ -13,12 +13,12 @@ def text_search():
     embedding.load("embedding.tar.gz")
 
     return titles , embedding
-
+#caching the data to access it faster on one session 
 titles, embedding =st.cache_data(text_search)()
 st.title("amazon product search")
 
 query=st.text_input("enter the query")
-
+#UI concept
 if st.button("search"):
     if query:
         result=embedding.search(query, 5)
